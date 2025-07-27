978369cf4847a567fe9891830994ad968f482e7f
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-07-27 11:22:04 +0330

    feat: implement home page ui and form validation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,73 @@
+"use client";
+
+import { useForm, Controller, useFormState } from "react-hook-form";
+import Container from "./components/Container";
+import Input from "./components/Input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { credentialsSchema, type Credentials } from "@/schema/credentials";
+import { useEffect } from "react";
+
 export default function HomePage() {
-  return null;
+  const {
+    handleSubmit: reactHookFormHandleSubmit,
+    control,
+    setFocus,
+  } = useForm<Credentials>({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+    resolver: zodResolver(credentialsSchema),
+  });
+
+  const { errors } = useFormState({
+    control,
+  });
+
+  useEffect(() => {
+    setFocus("username", { shouldSelect: true });
+  }, []);
+
+  function handleSubmit(values: Credentials) {
+    console.log(values);
+  }
+
+  return (
+    <Container className="flex items-center justify-center h-full">
+      <section className="flex flex-col justify-center items-center gap-y-8 w-96">
+        <h1>خوش آمدید! برای ادامه وارد شوید...</h1>
+        <form
+          className="flex flex-col bg-linear-to-r from-primary/70 via-primary/40 to-primary/70  p-8 rounded-xl w-full"
+          onSubmit={reactHookFormHandleSubmit(handleSubmit)}
+        >
+          <Controller
+            control={control}
+            render={({ field }) => (
+              <Input
+                {...field}
+                className="my-3 text-left bg-secondary"
+                placeholder="username"
+                errorText={errors.username?.message}
+              />
+            )}
+            name="username"
+          />
+          <Controller
+            control={control}
+            render={({ field }) => (
+              <Input
+                {...field}
+                className="my-3 text-left bg-secondary"
+                type="password"
+                placeholder="password"
+                errorText={errors.password?.message}
+              />
+            )}
+            name="password"
+          />
+          <button className="bg-tertiary p-1 rounded-lg mt-6 cursor-pointer">ورود</button>
+        </form>
+      </section>
+    </Container>
+  );
 }
