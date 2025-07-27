d65c6fee891bff7957d1175738656d6d77d95625
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-07-27 11:23:34 +0330

    feat: implement credentials schema

---
+++ src/schema/credentials.ts
@@ -0,0 +1,8 @@
+import {z} from "zod";
+
+export const credentialsSchema = z.object({
+  username: z.string().min(6, "نام کاربری وارد شده باید حداقل شامل 6 کاراکتر باشد."),
+  password: z.string().min(6, "رمز عبور وارد شده باید حداقل شامل 6 کاراکتر باشد.")
+})
+
+export type Credentials = z.infer<typeof credentialsSchema>;
