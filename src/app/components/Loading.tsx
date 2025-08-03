34ddcac1264fc2fb23cf45d9026a9ee50b063300
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-08-03 12:33:02 +0330

    feat: extract and share class name for loading component

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import clsx from "clsx";
+import React, { ComponentProps, PropsWithChildren } from "react";
 
-export default function Loading() {
-  return <section className="loader" />;
+type LoadingProps = Pick<ComponentProps<"section">, "className">;
+
+export default function Loading({ className }: LoadingProps) {
+  return <section className={clsx("loader", className)} />;
 }
