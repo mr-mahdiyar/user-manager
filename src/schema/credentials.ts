100de82c94d1c349d5601ae45473292af0a6cb2c
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-08-03 12:31:36 +0330

    debug: set min length for username to 4 characters

--- src/schema/credentials.ts
+++ src/schema/credentials.ts
@@ -1,7 +1,7 @@
 import {z} from "zod";
 
 export const credentialsSchema = z.object({
-  username: z.string().min(6, "نام کاربری وارد شده باید حداقل شامل 6 کاراکتر باشد."),
+  username: z.string().min(4, "نام کاربری وارد شده باید حداقل شامل 4 کاراکتر باشد."),
   password: z.string().min(6, "رمز عبور وارد شده باید حداقل شامل 6 کاراکتر باشد.")
 })
 
