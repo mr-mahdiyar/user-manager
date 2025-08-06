c26f402ae8ae7a5eca7d5992b6c96e70f4132345
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-08-06 15:28:25 +0330

    feat: implement add base page

---
+++ src/app/(protected)/dashboard/base/add/page.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { Controller, useForm } from "react-hook-form";
+import { Base, BaseSchema } from "@/schema/base";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@heroui/input";
+import Container from "@/components/Container";
+import { Button, addToast } from "@heroui/react";
+import { useAddBase } from "@/hooks/base";
+import { useEffect } from "react";
+
+export default function AddBasePage() {
+  const { addBase, wasAddBaseSuccessful, isAddBasePending, wasAddBaseFailure } = useAddBase();
+
+  const {
+    control,
+    reset,
+    handleSubmit: hookFormHandleSubmit,
+    formState: { errors },
+  } = useForm<Base>({
+    defaultValues: {
+      name: "",
+    },
+    resolver: zodResolver(BaseSchema),
+  });
+
+  useEffect(() => {
+    if (wasAddBaseSuccessful) {
+      addToast({
+        title: "عملیات موفق",
+        description: "مرجع با موفقیت اضافه شد.",
+        color: "success",
+      });
+    }
+  }, [wasAddBaseSuccessful]);
+
+  useEffect(() => {
+    if (wasAddBaseFailure) {
+      addToast({
+        title: "عملیات ناموفق",
+        description: "مرجع اضافه نشد.",
+        color: "danger",
+      });
+    }
+  }, [wasAddBaseFailure]);
+  async function handleSubmit(data: Base) {
+    try {
+      addBase(data);
+      if (wasAddBaseSuccessful) reset();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <Container className="border h-full w-full flex justify-center items-center rounded-3xl shadow-lg">
+      <form
+        className="w-96 flex flex-col gap-y-8 bg-cyan-400 p-12 rounded-lg"
+        onSubmit={hookFormHandleSubmit(handleSubmit)}
+      >
+        <p>نام مرجع را وارد کنید.</p>
+        <Controller
+          control={control}
+          render={({ field }) => (
+            <Input
+              {...field}
+              classNames={{
+                errorMessage: "mt-2 text-red-500 font-bold",
+              }}
+              placeholder="شهید رجایی لطف آباد"
+              errorMessage={errors.name?.message}
+              isInvalid={!!errors.name}
+            />
+          )}
+          name="name"
+        />
+        <Button color="danger" type="submit" disabled={isAddBasePending}>
+          {isAddBasePending ? "کمی صبر کنید..." : "تایید"}
+        </Button>
+      </form>
+    </Container>
+  );
+}
