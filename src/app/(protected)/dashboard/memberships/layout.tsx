39f3723ffe89d809363748727adfa07fddf9bcf5
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-08-06 14:05:46 +0330

    refactor: replace user page to specific route and separate root template and membership template

---
+++ src/app/(protected)/dashboard/memberships/layout.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Container from "@/components/Container";
+import { logout } from "@/services/auth";
+import { Button } from "@heroui/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { PropsWithChildren } from "react";
+
+import React from "react";
+
+export default function Layout({ children }: PropsWithChildren) {
+  const { replace } = useRouter();
+
+  async function clickHandler() {
+    await logout();
+    replace("/auth/login");
+  }
+
+  return (
+    <Container className="flex h-dvh gap-x-8 items-center p-6">
+      <nav className="flex flex-col gap-y-6 w-72 rounded-xl border bg-slate-200 p-4">
+        <Link href={"/dashboard/membership/add"} className="bg-lime-400 w-full block p-2 rounded-md text-center">
+          افزودن کاربر
+        </Link>
+        <Link href={"/dashboard/membership/search"} className="bg-amber-500 w-full block p-2 rounded-md text-center">
+          جست و جوی کاربر
+        </Link>
+        <Link href={"/dashboard/membership"} className="bg-primary/70 w-full block p-2 rounded-md text-center">
+          لیست کاربران
+        </Link>
+        <Link href={"/dashboard/base"} className="bg-fuchsia-400 w-full block p-2 rounded-md text-center">
+          مرجع ها
+        </Link>
+        <Button className="bg-rose-500 w-full block p-2 rounded-md cursor-pointer" onPressEnd={clickHandler}>
+          خروج
+        </Button>
+      </nav>
+      {children}
+    </Container>
+  );
+}
--- src/app/(protected)/template.tsx
+++ src/app/(protected)/template.tsx
@@ -1,43 +1,13 @@
 "use client";
 
-import Container from "@/components/Container";
-import { logout, refresh } from "@/services/auth";
-import { Button } from "@heroui/react";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { PropsWithChildren, useEffect } from "react";
+import { refresh } from "@/services/auth";
+import { useEffect, type PropsWithChildren } from "react";
 
-import React from "react";
-
-export default function template({ children }: PropsWithChildren) {
-  const { replace } = useRouter();
-
+export default function DashboardTemplate({ children }: PropsWithChildren) {
+  
   useEffect(() => {
     refresh();
   }, []);
 
-  async function clickHandler() {
-    await logout();
-    replace("/auth/login");
-  }
-
-  return (
-    <Container className="flex h-dvh gap-x-8 items-center p-6">
-      <nav className="flex flex-col gap-y-6 w-72 rounded-xl border bg-slate-200 p-4">
-        <Link href={"/dashboard/members/add"} className="bg-lime-400 w-full block p-2 rounded-md text-center">
-          افزودن کاربر
-        </Link>
-        <Link href={"/dashboard/members/search"} className="bg-amber-500 w-full block p-2 rounded-md text-center">
-          جست و جوی کاربر
-        </Link>
-        <Link href={"/dashboard/admin/edit"} className="bg-primary/70 w-full block p-2 rounded-md text-center">
-          ویرایش اطلاعات ادمین
-        </Link>
-        <Button className="bg-rose-500 w-full block p-2 rounded-md cursor-pointer" onPressEnd={clickHandler}>
-          خروج
-        </Button>
-      </nav>
-      {children}
-    </Container>
-  );
+  return <>{children}</>;
 }
