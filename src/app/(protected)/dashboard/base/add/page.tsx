4d896980a59643a36256446b435784b1af41bffc
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-08-09 11:02:39 +0330

    feat: implement location and leader inputs in add base form

--- src/app/(protected)/dashboard/base/add/page.tsx
+++ src/app/(protected)/dashboard/base/add/page.tsx
@@ -3,11 +3,11 @@
 import { Controller, useForm } from "react-hook-form";
 import { Base, BaseSchema } from "@/schema/base";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Input } from "@heroui/input";
-import  { PageContainer } from "@/components/Container";
+import { PageContainer } from "@/components/Container";
 import { Button, addToast } from "@heroui/react";
 import { useAddBase } from "@/hooks/base";
 import { useEffect } from "react";
+import { Input } from "@/components/Input";
 
 export default function AddBasePage() {
   const { addBase, wasAddBaseSuccessful, isAddBasePending, wasAddBaseFailure } = useAddBase();
@@ -20,6 +20,8 @@ export default function AddBasePage() {
   } = useForm<Base>({
     defaultValues: {
       name: "",
+      location: "",
+      leader: ""
     },
     resolver: zodResolver(BaseSchema),
   });
@@ -43,6 +45,7 @@ export default function AddBasePage() {
       });
     }
   }, [wasAddBaseFailure]);
+
   async function handleSubmit(data: Base) {
     try {
       addBase(data);
@@ -55,24 +58,51 @@ export default function AddBasePage() {
   return (
     <PageContainer className="flex justify-center items-center">
       <form
-        className="w-96 flex flex-col gap-y-8 bg-cyan-400 p-12 rounded-lg"
+        className="w-96 flex flex-col gap-y-6 bg-cyan-400 p-12 rounded-lg"
         onSubmit={hookFormHandleSubmit(handleSubmit)}
       >
-        <p>نام مرجع را وارد کنید.</p>
         <Controller
           control={control}
           render={({ field }) => (
             <Input
               {...field}
+              label="نام مرجع"
+              labelPlacement="outside"
               classNames={{
                 errorMessage: "mt-2 text-red-500 font-bold",
               }}
-              placeholder="شهید رجایی لطف آباد"
-              errorMessage={errors.name?.message}
+              placeholder="شهید رجایی"
+              errorMessage={errors.name?.message || ""}
               isInvalid={!!errors.name}
             />
           )}
           name="name"
+        />
+        <Controller
+          control={control}
+          render={({ field }) => (
+            <Input
+              label="روستا"
+              {...field}
+              placeholder="لطف آباد"
+              errorMessage={errors.location?.message || ""}
+              isInvalid={!!errors.location}
+            />
+          )}
+          name="location"
+        />
+        <Controller
+          control={control}
+          render={({ field }) => (
+            <Input
+              label="رئیس مرجع"
+              {...field}
+              placeholder="کاظم غلامرضایی"
+              errorMessage={errors.leader?.message || ""}
+              isInvalid={!!errors.leader}
+            />
+          )}
+          name="leader"
         />
         <Button color="danger" type="submit" disabled={isAddBasePending}>
           {isAddBasePending ? "کمی صبر کنید..." : "تایید"}
