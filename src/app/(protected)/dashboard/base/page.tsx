5ea21f541ee11308ad9de78eb58bf0a454dd1efb
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-08-09 11:03:23 +0330

    refactor: remove duplicate codes and import add base page template

--- src/app/(protected)/dashboard/base/page.tsx
+++ src/app/(protected)/dashboard/base/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import Container, { PageContainer } from "@/components/Container";
+import { PageContainer } from "@/components/Container";
+import PageTemplate from "@/components/ui/base/add/PageTemplate";
 import { useBases } from "@/hooks/base";
 import { Spinner } from "@heroui/react";
 
@@ -9,29 +10,32 @@ export default function BasesListPage() {
 
   if (isFetchingBases) {
     return (
-      <PageContainer>
-        <Container className="flex justify-center items-center h-full">
-          <Spinner color="primary" />
-        </Container>
-      </PageContainer>
+      <PageTemplate>
+        <Spinner color="primary" />
+      </PageTemplate>
     );
   }
 
   if (wasFetchingBasesFailure) {
     return (
-      <PageContainer>
-        <Container className="flex justify-center items-center h-full">
-          <p className="text-red-500">متاسفانه دریافت مرجع ها با خطا رو به رو شد.</p>
-        </Container>
-      </PageContainer>
+      <PageTemplate>
+        <p className="text-red-500">متاسفانه دریافت مرجع ها با خطا رو به رو شد.</p>
+      </PageTemplate>
     );
   }
+
+  if (bases?.length === 0)
+    return (
+      <PageTemplate>
+        <p className="text-red-500">هنوز مرجعی اضافه نشده است.</p>
+      </PageTemplate>
+    );
 
   return (
     <PageContainer className="flex flex-col gap-y-8 p-12">
       {bases?.map((base) => (
-        <section key={base.id} className="border p-6">
-          
+        <section key={base.id} className="border rounded-md p-6 flex justify-between">
+          <p>{base.name}</p>
         </section>
       ))}
     </PageContainer>
