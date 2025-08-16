795997eae1239b2f89fd095ee51f2f2cc3211daf
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-08-16 17:21:10 +0330

    debug: remove id from User model

---
+++ prisma/migrations/20250816114300_delete_id_field_from_user_model/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `id` on the `User` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "nationalCode" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "family" TEXT NOT NULL,
+    "fatherName" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "birthDate" DATETIME NOT NULL,
+    "membershipDate" DATETIME,
+    "caseNumber" TEXT NOT NULL,
+    "statusId" INTEGER NOT NULL,
+    "baseId" INTEGER NOT NULL,
+    CONSTRAINT "User_statusId_fkey" FOREIGN KEY ("statusId") REFERENCES "UserStatus" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "User_baseId_fkey" FOREIGN KEY ("baseId") REFERENCES "Base" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_User" ("baseId", "birthDate", "caseNumber", "family", "fatherName", "membershipDate", "name", "nationalCode", "phone", "statusId") SELECT "baseId", "birthDate", "caseNumber", "family", "fatherName", "membershipDate", "name", "nationalCode", "phone", "statusId" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_nationalCode_key" ON "User"("nationalCode");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
