49d5a13eced89843a4007a459c3cc4a5b59929eb
Mahdiyar Poursalari <m.poursalari3200@gmail.com>
2025-10-27 15:12:16 +0330

    update: rewrite the navigation address from home page

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,7 +15,7 @@ export async function middleware(request: NextRequest) {
       }
 
       if (isPathMatchedWithAuthPaths(currentPath) || isHomePage(currentPath)) {
-        return NextResponse.redirect(new URL("/dashboard", request.url));
+        return NextResponse.redirect(new URL("/dashboard/memberships", request.url));
       }
       return;
     }
